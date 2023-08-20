5410e3a842ef599a8eef3b9a2b28f89507a501de
arup-paul <aruppaulctg@gmail.com>
2023-08-20 21:11:52 +0600

    migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\AccountType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('account_type',['Individual','Business'])->default('Individual');
+            $table->enum('account_type',[AccountType::BUSINESS, AccountType::INDIVIDUAL])->default(AccountType::INDIVIDUAL);
             $table->double('balance')->default(0);
             $table->string('email')->unique();
             $table->string('password');
